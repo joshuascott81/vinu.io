9a77b36f2634387894c5a83616eac4ec36295f9f
Joshua Scott <joshuascott81@gmail.com>
2018-07-23 10:09:08 -0500

    Finished initial google oauth setup

--- server/server.js
+++ server/server.js
@@ -1,10 +1,32 @@
 const express = require("express");
+
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ hi: "there" });
-});
+// Handshake between Express/Passport and Google Strategy
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken);
+    }
+  )
+);
 
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
